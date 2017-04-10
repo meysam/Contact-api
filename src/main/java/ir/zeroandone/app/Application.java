0454fe21d8217263d4921ca66a032400dc20429f
meysam <meysam.tamkin@gmail.com>
2017-04-10 13:58:53 +0430

    added resource bundle message source

--- src/main/java/ir/zeroandone/app/Application.java
+++ src/main/java/ir/zeroandone/app/Application.java
@@ -1,22 +1,49 @@
 package ir.zeroandone.app;
 
-import ir.zeroandone.app.domain.Person;
 import ir.zeroandone.app.domain.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application  {
+public class Application extends WebMvcConfigurerAdapter {
 
     @Autowired
     private PersonRepository repository;
 
+    @Autowired
+    MessageSource messageSource;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean(name = "validator")
+    public LocalValidatorFactoryBean validator()
+    {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
+    @Override
+    public Validator getValidator()
+    {
+        return validator();
     }
 
   /*  @Override
